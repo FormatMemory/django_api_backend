c687ccd8a15af35d7b0d3174bda9b3d9c7a65bbd
Yusheng Ding <davidthinkleding@Gmail.com>
2019-03-28 20:22:18 -0700

    change __init__ stop importing test data for now

--- app/v1/__init__.py
+++ app/v1/__init__.py
@@ -5,8 +5,9 @@ ENVIRONMENT = os.environ.get('ENVIRONMENT', 'local')
 if ENVIRONMENT == 'production':
     import pymysql
     pymysql.install_as_MySQLdb()
-else:
-    import subprocess
-    bashCommand = "bash ../scripts/load_sample_data.sh"
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
+# else:
+#     import subprocess
+#     os.chdir("../")
+#     bashCommand = "bash scripts/load_sample_data.sh"
+#     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+#     output, error = process.communicate()
