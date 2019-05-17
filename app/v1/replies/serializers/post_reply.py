f3f965ecb19d79693aee83a3778cda4de448815b
Yusheng Ding <davidthinkleding@Gmail.com>
2019-05-16 17:58:45 -0700

    add post reply votes count to post reply

--- app/v1/replies/serializers/post_reply.py
+++ app/v1/replies/serializers/post_reply.py
@@ -1,15 +1,25 @@
 from rest_framework import serializers
 from v1.accounts.serializers.user import UserSerializer
 from v1.replies.models.post_reply import PostReply
+from v1.votes.models.post_reply_vote import PostReplyVote
 
 
 class PostReplySerializer(serializers.ModelSerializer):
     user = UserSerializer()
+    post_reply_upper_votes_count = serializers.SerializerMethodField()
+    post_reply_down_votes_count = serializers.SerializerMethodField()
 
     class Meta:
         model = PostReply
         fields = '__all__'
 
+    @staticmethod
+    def get_post_reply_down_votes_count(post_reply):
+        return PostReplyVote.objects.filter(post_reply=post_reply, value=-1).count()
+
+    @staticmethod
+    def get_post_reply_upper_votes_count(post_reply):
+        return PostReplyVote.objects.filter(post_reply=post_reply, value=1).count()
 
 class PostReplySerializerCreate(serializers.ModelSerializer):
 
