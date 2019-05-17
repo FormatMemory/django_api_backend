8c6904011b4107d3df59e3f9c0fb8d8915870354
Yusheng Ding <davidthinkleding@Gmail.com>
2019-05-17 15:12:12 -0700

    change post in single page of postlist to be 3

--- app/v1/posts/views/post.py
+++ app/v1/posts/views/post.py
@@ -14,7 +14,7 @@ from v1.accounts.models.user import User
 from datetime import datetime
 
 class PostPagination(PageNumberPagination):
-    page_size = 5
+    page_size = 3
     page_size_query_param = 'page_size'
     max_page_size = 50
 
