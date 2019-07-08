4d41111e4e5f482cc88144fb59edf5749b1c8f59
Yusheng Ding <davidthinkleding@Gmail.com>
2019-07-08 16:58:25 -0700

    add / to make it work

--- app/v1/posts/urls.py
+++ app/v1/posts/urls.py
@@ -5,7 +5,7 @@ from .views.post import PostListView, PostCreateAPIView, PostDetail
 urlpatterns = [
 
     # Posts
-    url(r'^posts$', PostListView.as_view()),
+    url(r'^posts/$', PostListView.as_view()),
     url(r'^posts_create$', PostCreateAPIView.as_view()), 
     url(r'^posts/(?P<post_id>[\d]+)$', PostDetail.as_view()),
 
