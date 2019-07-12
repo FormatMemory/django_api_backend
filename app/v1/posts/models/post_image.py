608f912dcef0b4947efb6e67806af6f49d3956cb
Yusheng Ding <davidthinkleding@Gmail.com>
2019-07-11 22:08:08 -0700

    fix bug post image

--- app/v1/posts/models/post_image.py
+++ app/v1/posts/models/post_image.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 from v1.posts.models.post import Post
-from v1.general.created_modified import CreatedModifie
+from v1.general.created_modified import CreatedModified
 from v1.general.image_validator import validate_image
 import uuid
 import os
@@ -11,9 +11,9 @@ def get_file_path(instance, filename):
     filename = "%s.%s" % (uuid.uuid4(), ext)
     return os.path.join('images/', filename)
 
-class PostImage(CreatedModifie):
+class PostImage(CreatedModified):
     id = models.AutoField(primary_key=True)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, Null=False, blank=False)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=False, blank=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
     image = models.ImageField(blank=True, upload_to=get_file_path, validators=[validate_image])
 
--- app/v1/posts/urls.py
+++ app/v1/posts/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from .views.post import PostListView, PostCreateAPIView, PostDetail
+from .views.post_image import PostImageDetailAPIView
 
 
 urlpatterns = [
@@ -8,5 +9,6 @@ urlpatterns = [
     url(r'^posts/$', PostListView.as_view()),
     url(r'^posts_create/$', PostCreateAPIView.as_view()),
     url(r'^posts/(?P<post_id>[\d]+)$', PostDetail.as_view()),
+    url(r'^post_image/(?P<pk>[\d]+)$', PostImageDetailAPIView.as_view()),
 
 ]
